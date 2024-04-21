5e3244a0afa5a5195ce5d85415189bb404a0096c
Eduard Gavril <a58616296177@gmail.com>
2024-04-22 01:06:47 +0200

    supadata
    aggiunta database con utente abilitato al login

---
+++ LogSupaData.js
@@ -0,0 +1,28 @@
+// Configura il client Supabase
+const supabaseUrl = 'https://uuusektiafxceukkpxdc.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1dXNla3RpYWZ4Y2V1a2tweGRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM2OTA1MDcsImV4cCI6MjAyOTI2NjUwN30.sWc1CVbnIUNaqzD8lFaORs-7LhZlYSqj5_CenrXraUI';
+const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
+
+// Gestisci l'evento di login
+document.getElementById('loginButton').addEventListener('click', async (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    //verifica le credenziali
+    const { data, error } = await supabaseClient.from('authorized').select().eq('email', email).eq('password', password);
+    
+    if (error) {
+        console.error(error.message);
+        return;
+    }
+
+    if (data.length > 0) {
+        console.log('Login successful');
+        window.location.href = 'giusto.html'; // Reindirizza alla pagina successiva
+    } else {
+        console.error('Invalid credentials');
+        window.location.href = 'fallito.html'; // Reindirizza alla pagina successiva
+    }
+});
--- login.js
+++ login.js
@@ -6,15 +6,13 @@ const passwordInput = document.getElementById('password');
 
 // Funzione per gestire il click sull'icona "toggle-password"
 function togglePasswordVisibility() {
-    // Cambia il tipo di input della password
+    
     if (passwordInput.type === 'password') {
         passwordInput.type = 'text';
-        // Cambia l'icona per indicare che la password è visibile
-        togglePasswordIcon.src = './img/view.png'; // Assicurati di avere un'icona appropriata per indicare la password nascosta
+        togglePasswordIcon.src = './img/view.png'; 
     } else {
         passwordInput.type = 'password';
-        // Cambia l'icona per indicare che la password è nascosta
-        togglePasswordIcon.src = './img/hidden.png'; // Assicurati di avere un'icona appropriata per indicare la password visibile
+        togglePasswordIcon.src = './img/hidden.png'; 
     }
 }
 
