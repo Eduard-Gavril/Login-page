63277334a3ca4274b386a349d81d53f726946233
Eduard Gavril <a58616296177@gmail.com>
2024-04-26 14:08:56 +0200

    up
    up

--- login.js
+++ login.js
@@ -1,22 +1,24 @@
-// Seleziona l'elemento dell'icona "toggle-password"
-const togglePasswordIcon = document.querySelector('.toggle-password');
+document.addEventListener('DOMContentLoaded', function() {
+    // Seleziona l'elemento dell'icona "toggle-password"
+    const togglePasswordIcon = document.querySelector('.toggle-password');
 
-// Seleziona l'input della password
-const passwordInput = document.getElementById('password');
+    // Seleziona l'input della password
+    const passwordInput = document.getElementById('password');
 
-// Funzione per gestire il click sull'icona "toggle-password"
-function togglePasswordVisibility() {
-    
-    if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        togglePasswordIcon.src = './img/view.png'; 
-    } else {
-        passwordInput.type = 'password';
-        togglePasswordIcon.src = './img/hidden.png'; 
+    // Funzione per gestire il click sull'icona "toggle-password"
+    function togglePasswordVisibility() {
+        // Cambia il tipo di input della password
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            // Cambia l'icona per indicare che la password è visibile
+            togglePasswordIcon.className = 'ri-eye-fill'; // Assicurati di avere un'icona appropriata per indicare la password nascosta
+        } else {
+            passwordInput.type = 'password';
+            // Cambia l'icona per indicare che la password è nascosta
+            togglePasswordIcon.className = 'ri-eye-off-fill'; // Assicurati di avere un'icona appropriata per indicare la password visibile
+        }
     }
-}
 
-// Aggiungi l'event listener per il click sull'icona "toggle-password"
-togglePasswordIcon.addEventListener('click', togglePasswordVisibility);
-
-
+    // Aggiungi l'event listener per il click sull'icona "toggle-password"
+    togglePasswordIcon.addEventListener('click', togglePasswordVisibility);
+});
